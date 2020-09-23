2b7f7b0874e95e4c423c2408f82874d63af110e9
Simon VDB <simonvdbroeck@gmail.com>
2020-09-23 13:33:49 +0200

    fix pkg json

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
 export { hydrate, render, registerComponent } from "./react-across";
 export { AcrossComponent } from "./react-across.utils";
 export { useAcrossPartial, AcrossPartial } from "./AcrossPartial";
+export { StaticContent, AcrossWebResource } from "./thymeleaf";
